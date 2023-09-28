0da4d2d16ccf078d45c0b70f741ba6b37c4ab7fc
tony-brian <btoni3823@gmail.com>
2023-09-28 14:49:23 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,61 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+      """
+    Represents a resource for managing a list of plants.
+    This resource supports HTTP GET and POST methods for retrieving and creating plant data.
+    """
+def get(self):
+        """
+        Retrieve a list of all plants.
+        Returns:
+            Response: A JSON response containing a list of plant data.
+        """
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
 
+def post(self):
+        """
+        Create a new plant.
+        Returns:
+            Response: A JSON response containing the newly created plant data.
+        """
+
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+
+
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
-        
+      """
+    Represents a resource for managing an individual plant by ID.
+    This resource supports HTTP GET method for retrieving a specific plant by its ID.
+    """
+
+def get(self, num):
+        """
+        Retrieve a specific plant by its ID.
+        Args:
+            num (int): The unique identifier of the plant.
+        Returns:
+            Response: A JSON response containing the plant data for the specified ID.
+        """
+        plant = Plant.query.filter_by(id=num).first().to_dict()
+        return make_response(jsonify(plant), 200)
+
+
+api.add_resource(PlantByID, '/plants/<int:num>')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
